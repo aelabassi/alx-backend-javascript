b8bddadbe456049da3f7a3bb1d35f09b69dd156b
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-09-30 16:28:44 +0100

    for...of

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const value of array) {
     const idx = array.indexOf(value);
-    array[idx] = appendString + value;
+    array[idx] = appendString + value; // eslint-disable-line no-param-reassign
   }
 
   return array;
