4a8ac50ed057fa201867cdd31fb7299c82f87c98
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-09-30 11:45:40 +0100

    using const and let

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,17 +1,17 @@
-export function taskFirst(){
-    // using const
-    const task = 'I prefer const when I can.';
-    return task;
+export function taskFirst() {
+  // using const
+  const task = 'I prefer const when I can.';
+  return task;
 }
 
-export function getLast(){
-    return ' is okay';
+export function getLast() {
+  return ' is okay';
 }
 
-export function taskNext(){
-    // using let
-    let combination = 'But sometimes let';
-    combination += getLast();
+export function taskNext() {
+  // using let
+  let combination = 'But sometimes let';
+  combination += getLast();
 
-    return combination;
+  return combination;
 }
