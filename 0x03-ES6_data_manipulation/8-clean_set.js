b339c7f965405285a9d829e1f9bdc33be01393fa
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-10-08 11:39:57 +0100

    weak maps

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,8 @@
 export default function cleanSet(set, startString) {
+  const restString = [];
   if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
-  const restString = [];
   for (const value of set.values()) {
     if (value.startsWith(startString) && typeof value === 'string') {
       const subString = value.substring(startString.length);
