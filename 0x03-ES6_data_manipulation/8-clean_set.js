29b2aa662500a841219532f9653f957f84d2b824
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-10-08 10:32:38 +0100

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,9 @@
 export default function cleanSet(set, startString) {
-  // Check if the set is not empty and the startString is not empty
-  // and the set is an instance of Set and the startString is a string
   if (!set && !startString && !(set instanceof Set) && typeof startString !== 'string') {
     return '';
   }
-
   const restString = [];
-  // Iterate over the set
   for (const value of set.values()) {
-    // Check if the value starts with the startString and the startString is a string
     if (value.startsWith(startString) && typeof value === 'string') {
       const subString = value.substring(startString.length);
       if (subString !== value) {
@@ -16,7 +11,5 @@ export default function cleanSet(set, startString) {
       }
     }
   }
-
-  // Return the new set
   return restString.join('-');
 }
