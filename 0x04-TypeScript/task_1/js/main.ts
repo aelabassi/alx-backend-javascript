2c02d53ab2732317bac1a14befea1dc3be00630f
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-10-09 12:07:18 +0100

    class implement a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,4 +1,4 @@
-export default interface Teacher {
+export interface Teacher {
     readonly firstName: string;
     readonly lastName: string;
     fullTimeEmployee: boolean;
@@ -7,15 +7,44 @@ export default interface Teacher {
     [propName: string]: any;
 }
 
-interface Directors extends Teacher {
+export interface Directors extends Teacher {
     numberOfReports: number;
 }
 
-function printTeacher(firstName: string, lastName: string): string {
+export function printTeacher(firstName: string, lastName: string): string {
     return `${firstName.charAt(0)}. ${lastName}`;
 }
 
-interface printTeacherFunction {
+export interface printTeacherFunction {
     (firstName: string, lastName: string): string;
 }
+export interface StudentInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
 
+export interface StudentConstructor {
+    new(firstName: string, lastName: string): StudentInterface;
+}
+
+export class StudentClass implements StudentInterface {
+    private _firstName: string;
+    private _lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this._firstName = firstName;
+        this._lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this._firstName;
+    }
+}
+
+export function studentInstance(student: StudentConstructor, firstName: string, lastName: string): StudentInterface {
+    return new student(firstName, lastName);
+}
