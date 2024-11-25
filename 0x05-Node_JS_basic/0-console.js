43f3680b9019f00dbd553b05027ae53bb775e2c0
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-11-25 15:06:24 +0100

    executing basic JS with node.js #0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,8 @@
+// In the file 0-console.js, create a function
+// named displayMessage that prints in STDOUT the string argument.
+
+function displayMessage(message) {
+    console.log(message);
+}
+
+module.exports = displayMessage;
