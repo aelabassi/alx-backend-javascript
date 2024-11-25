96f6796b43c0f0e08a8538400c34bddc41a84662
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-11-25 16:30:53 +0100

    small http server using node's http module #4

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,16 @@
+// simple node.js server
+// run this file with node 4-http.js
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+module.exports = app;
