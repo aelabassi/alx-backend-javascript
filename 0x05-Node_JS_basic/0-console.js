6aacc24f0f5ae38dd4a7e718645a9942398da7d7
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-11-25 15:16:16 +0100

    executing basic JS with node.js #0

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -2,7 +2,7 @@
 // named displayMessage that prints in STDOUT the string argument.
 
 function displayMessage(message) {
-    console.log(message);
+  console.log(message);
 }
 
 module.exports = displayMessage;
