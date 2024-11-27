f1a48904fd63bcf5a9f96353c98a46490d7ae800
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-11-27 16:10:17 +0100

    basic test #0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,22 +1,22 @@
 const assert = require('assert');
-const calculNumber = require('./0-calcul');
+const calculateNumber = require('./0-calcul');
 
 describe('calculNumber', () => {
   describe('#round', () => {
     it('should return 3', () => {
-      assert.strictEqual(calculNumber(1.2, 2.2), 3);
+      assert.strictEqual(calculateNumber(1.2, 2.2), 3);
     });
     it('should return 6', () => {
-      assert.strictEqual(calculNumber(1.5, 3.5), 6);
+      assert.strictEqual(calculateNumber(1.5, 3.5), 6);
     });
     it('should return 5', () => {
-      assert.strictEqual(calculNumber(1.5, 3.4), 5);
+      assert.strictEqual(calculateNumber(1.5, 3.4), 5);
     });
     it('should return 9', () => {
-      assert.strictEqual(calculNumber(4.3, 5.3), 9);
+      assert.strictEqual(calculateNumber(4.3, 5.3), 9);
     });
     it('should return 3', () => {
-      assert.strictEqual(calculNumber(1.499999, 2.49999), 3);
+      assert.strictEqual(calculateNumber(1.499999, 2.49999), 3);
     });
   });
 });
