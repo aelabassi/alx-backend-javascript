5961830ed9050d4baa015caed45a33d2e19080e8
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-11-27 12:12:17 +0100

    basic test with mocha #0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,19 +1,22 @@
 const assert = require('assert');
-const calculNumber = require('./0-calcul.js');
+const calculNumber = require('./0-calcul');
 
 describe('calculNumber', () => {
-    describe('#round', () => {
-        it('should return 3', () => {
-            assert.strictEqual(calculNumber(1.2, 2.2), 3);
-        });
-        it('should return 5', () => {
-            assert.strictEqual(calculNumber(1.5, 3.5), 5);
-        });
-        it('should return 5', () => {
-            assert.strictEqual(calculNumber(1.5, 3.4), 5);
-        });
-        it('it should return 10', () => {
-            assert.strictEqual(calculNumber(4.3, 5.3), 10);
-        })
+  describe('#round', () => {
+    it('should return 3', () => {
+      assert.strictEqual(calculNumber(1.2, 2.2), 3);
     });
+    it('should return 6', () => {
+      assert.strictEqual(calculNumber(1.5, 3.5), 6);
+    });
+    it('should return 5', () => {
+      assert.strictEqual(calculNumber(1.5, 3.4), 5);
+    });
+    it('should return 9', () => {
+      assert.strictEqual(calculNumber(4.3, 5.3), 9);
+    });
+    it('should return 3', () => {
+        assert.strictEqual(calculNumber(1.499999, 2.49999), 3);
+    })
+  });
 });
