59cefd849a6672e9b6b83656c13775be52b18ff1
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-11-28 12:32:51 +0100

    basic test: tetsing a server responses #8

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -7,6 +7,11 @@ app.get('/', (req, res) => {
     res.send('Welcome to the payment system');
 })
 
+app.get('/cart/:id(\\d+)', (req, res) => {
+    res.send(`Payment methods for cart ${req.params.id}`);
+})
+
+
 app.listen(PORT, () => {
     console.log(`API available on localhost port ${PORT}`);
 });
