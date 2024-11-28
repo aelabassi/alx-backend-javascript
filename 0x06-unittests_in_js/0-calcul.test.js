6ee473e754cb6509c4459958ca09d959072911ba
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-11-28 10:10:06 +0100

    basic test using chai #2

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,26 +2,25 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-
-    it('should return 3', () => {
-      assert.strictEqual(calculateNumber(1.2, 2.2), 3);
-    });
-    it('should return 6', () => {
-      assert.strictEqual(calculateNumber(1.5, 3.5), 6);
-    });
-    it('should return 5', () => {
-      assert.strictEqual(calculateNumber(1.5, 3.4), 5);
-    });
-    it('should return 9', () => {
-      assert.strictEqual(calculateNumber(4.3, 5.3), 9);
-    });
-    it('should return 3', () => {
-      assert.strictEqual(calculateNumber(1.499999, 2.49999), 3);
-    });
-    it('should return 4', () => {
-      assert.strictEqual(calculateNumber(1.4, 3.4), 4);
-    });
-    it('should return -2', () => {
-      assert.strictEqual(calculateNumber(0.5, -3), -2);
-    });
+  it('should return 3', () => {
+    assert.strictEqual(calculateNumber(1.2, 2.2), 3);
+  });
+  it('should return 6', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.5), 6);
+  });
+  it('should return 5', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.4), 5);
+  });
+  it('should return 9', () => {
+    assert.strictEqual(calculateNumber(4.3, 5.3), 9);
+  });
+  it('should return 3', () => {
+    assert.strictEqual(calculateNumber(1.499999, 2.49999), 3);
+  });
+  it('should return 4', () => {
+    assert.strictEqual(calculateNumber(1.4, 3.4), 4);
+  });
+  it('should return -2', () => {
+    assert.strictEqual(calculateNumber(0.5, -3), -2);
+  });
 });
--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,37 +1,36 @@
-const assert = require('assert');
 const { expect } = require('chai');
-const calculateNumber = require('./1-calcul');
+const calculateNumber = require('./1-calcul_chai');
 
 describe('calculateNumber', () => {
-  describe('SUM', () => {
+  describe('sUM', () => {
     it('should return the sum of two numbers', () => {
       expect(calculateNumber('SUM', 1, 3)).to.equal(4);
       expect(calculateNumber('SUM', 1.5, 3.5)).to.equal(6);
       expect(calculateNumber('SUM', 1.4, 3.5)).to.equal(5);
     });
   });
-  describe('SUBSTRACT', () => {
+  describe('sUBSTRACT', () => {
     it('should return the substraction of two numbers', () => {
-        expect(calculateNumber('SUBSTRACT', 1, 3)).to.equal(-2);
-        expect(calculateNumber('SUBSTRACT', 1.5, 3.5)).to.equal(-2);
-        expect(calculateNumber('SUBSTRACT', 1.4, 3.5)).to.equal(-3);
+      expect(calculateNumber('SUBSTRACT', 1, 3)).to.equal(-2);
+      expect(calculateNumber('SUBSTRACT', 1.5, 3.5)).to.equal(-2);
+      expect(calculateNumber('SUBSTRACT', 1.4, 3.5)).to.equal(-3);
     });
   });
-  describe('DIVIDE', () => {
+  describe('dIVIDE', () => {
     it('should return the division of two numbers', () => {
-        expect(calculateNumber('DIVIDE', 4, 2)).to.equal(2);
-        expect(calculateNumber('DIVIDE', 1.5, 3.5)).to.equal(0.5);
-        expect(calculateNumber('DIVIDE', 3.5, 1.5)).to.equal(2);
+      expect(calculateNumber('DIVIDE', 4, 2)).to.equal(2);
+      expect(calculateNumber('DIVIDE', 1.5, 3.5)).to.equal(0.5);
+      expect(calculateNumber('DIVIDE', 3.5, 1.5)).to.equal(2);
     });
     it('should return "Error" if the second number is 0', () => {
-        expect(calculateNumber('DIVIDE', 4, 0)).to.equal('Error');
+      expect(calculateNumber('DIVIDE', 4, 0)).to.equal('Error');
     });
   });
   describe('other', () => {
     it('should return 0 if the type is not SUM, SUBSTRACT or DIVIDE', () => {
-        expect(calculateNumber('MULT', 1, 3)).to.equal(0);
-        expect(calculateNumber('NORM', 1.5, 3.5)).to.equal(0);
-        expect(calculateNumber('UNIT', 1.4, 3.5)).to.equal(0);
+      expect(calculateNumber('MULT', 1, 3)).to.equal(0);
+      expect(calculateNumber('NORM', 1.5, 3.5)).to.equal(0);
+      expect(calculateNumber('UNIT', 1.4, 3.5)).to.equal(0);
     });
   });
 });
